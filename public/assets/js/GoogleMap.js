af587edc4077c0baa0d0f30c83cf7a9d8d6940f2
hayate242 <sippu242@gmail.com>
2018-11-30 17:29:02 +0900

    first commit Added google map

---
+++ public/assets/js/GoogleMap.js
@@ -0,0 +1,10 @@
+
+function initMap() {
+  var mapPosition = {lat: 35.170662, lng: 136.923430}
+  var mapArea = document.getElementById('maps');
+  var mapOptions = {
+    center: mapPosition,
+    zoom: 16,
+  };
+  var map = new google.maps.Map(mapArea, mapOptions);
+}
