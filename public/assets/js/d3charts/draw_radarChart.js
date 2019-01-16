0152e254c26645c38984d26d16b849b050d44f70
hayate242 <sippu242@gmail.com>
2019-01-17 02:33:57 +0900

    radar chart表示完了

--- public/assets/js/d3charts/draw_radarChart.js
+++ public/assets/js/d3charts/draw_radarChart.js
@@ -1,36 +1,72 @@
 
 
 function draw_radar_chart( crane_id ){
-	var w = 500,
-		h = 500;
+	var w = 600,
+		h = 600;
 	
 	var colorscale = d3.scaleOrdinal(d3.schemeCategory10);
 
 	//Legend titles
-	var LegendOptions = ['Smartphone','Tablet'];
+	var LegendOptions = ['0-10%','10-50%','50-63%','63-80%','80-100%','100%以上'];
 	
 	//Data
 	const index = calc_index(crane_id);
 	console.log(crane_data[index]._0_10);
 	var d = [
 				[
-				{axis:"A",value: Number(crane_data[index]._0_10)},
-				{axis:"B",value: Number(crane_data[index+1]._0_10)},
-				{axis:"C",value: Number(crane_data[index+2]._0_10)},
-				{axis:"D",value: Number(crane_data[index+3]._0_10)},
-				{axis:"E",value: Number(crane_data[index+4]._0_10)},
-				{axis:"F",value: Number(crane_data[index+5]._0_10)},
-				{axis:"G",value: Number(crane_data[index+6]._0_10)},
-				{axis:"H",value: Number(crane_data[index+7]._0_10)}
+					{axis:"A",value: Number(crane_data[index]._0_10)},
+					{axis:"B",value: Number(crane_data[index+1]._0_10)},
+					{axis:"C",value: Number(crane_data[index+2]._0_10)},
+					{axis:"D",value: Number(crane_data[index+3]._0_10)},
+					{axis:"E",value: Number(crane_data[index+4]._0_10)},
+					{axis:"F",value: Number(crane_data[index+5]._0_10)},
+					{axis:"G",value: Number(crane_data[index+6]._0_10)},
+					{axis:"H",value: Number(crane_data[index+7]._0_10)}
 				],[
-				{axis:"A",value: Number(crane_data[index]._0_10)},
-				{axis:"B",value: Number(crane_data[index+1]._0_10)},
-				{axis:"C",value: Number(crane_data[index+2]._0_10)},
-				{axis:"D",value: Number(crane_data[index+3]._0_10)},
-				{axis:"E",value: Number(crane_data[index+4]._0_10)},
-				{axis:"F",value: Number(crane_data[index+5]._0_10)},
-				{axis:"G",value: Number(crane_data[index+6]._0_10)},
-				{axis:"H",value: Number(crane_data[index+7]._0_10)}
+					{axis:"A",value: Number(crane_data[index]._10_50)},
+					{axis:"B",value: Number(crane_data[index+1]._10_50)},
+					{axis:"C",value: Number(crane_data[index+2]._10_50)},
+					{axis:"D",value: Number(crane_data[index+3]._10_50)},
+					{axis:"E",value: Number(crane_data[index+4]._10_50)},
+					{axis:"F",value: Number(crane_data[index+5]._10_50)},
+					{axis:"G",value: Number(crane_data[index+6]._10_50)},
+					{axis:"H",value: Number(crane_data[index+7]._10_50)}
+				],[
+					{axis:"A",value: Number(crane_data[index]._50_63)},
+					{axis:"B",value: Number(crane_data[index+1]._50_63)},
+					{axis:"C",value: Number(crane_data[index+2]._50_63)},
+					{axis:"D",value: Number(crane_data[index+3]._50_63)},
+					{axis:"E",value: Number(crane_data[index+4]._50_63)},
+					{axis:"F",value: Number(crane_data[index+5]._50_63)},
+					{axis:"G",value: Number(crane_data[index+6]._50_63)},
+					{axis:"H",value: Number(crane_data[index+7]._50_63)}
+				],[
+					{axis:"A",value: Number(crane_data[index]._63_80)},
+					{axis:"B",value: Number(crane_data[index+1]._63_80)},
+					{axis:"C",value: Number(crane_data[index+2]._63_80)},
+					{axis:"D",value: Number(crane_data[index+3]._63_80)},
+					{axis:"E",value: Number(crane_data[index+4]._63_80)},
+					{axis:"F",value: Number(crane_data[index+5]._63_80)},
+					{axis:"G",value: Number(crane_data[index+6]._63_80)},
+					{axis:"H",value: Number(crane_data[index+7]._63_80)}
+				],[
+					{axis:"A",value: Number(crane_data[index]._80_100)},
+					{axis:"B",value: Number(crane_data[index+1]._80_100)},
+					{axis:"C",value: Number(crane_data[index+2]._80_100)},
+					{axis:"D",value: Number(crane_data[index+3]._80_100)},
+					{axis:"E",value: Number(crane_data[index+4]._80_100)},
+					{axis:"F",value: Number(crane_data[index+5]._80_100)},
+					{axis:"G",value: Number(crane_data[index+6]._80_100)},
+					{axis:"H",value: Number(crane_data[index+7]._80_100)}
+				],[
+					{axis:"A",value: Number(crane_data[index]._100_over)},
+					{axis:"B",value: Number(crane_data[index+1]._100_over)},
+					{axis:"C",value: Number(crane_data[index+2]._100_over)},
+					{axis:"D",value: Number(crane_data[index+3]._100_over)},
+					{axis:"E",value: Number(crane_data[index+4]._100_over)},
+					{axis:"F",value: Number(crane_data[index+5]._100_over)},
+					{axis:"G",value: Number(crane_data[index+6]._100_over)},
+					{axis:"H",value: Number(crane_data[index+7]._100_over)}
 				]
 			];
 
@@ -45,13 +81,13 @@ function draw_radar_chart( crane_id ){
 
 	//Call function to draw the Radar chart
 	//Will expect that data is in %'s
-	RadarChart.draw("#chart", d, mycfg);
+	RadarChart.draw("#radar_chart", d, mycfg);
 
 	////////////////////////////////////////////
 	/////////// Initiate legend ////////////////
 	////////////////////////////////////////////
 
-	var svg = d3.select('#body')
+	var svg = d3.select('#radar_chart_txt')
 		.selectAll('svg')
 		.append('svg')
 		.attr("width", w+300)
@@ -79,7 +115,7 @@ function draw_radar_chart( crane_id ){
 			.data(LegendOptions)
 			.enter()
 			.append("rect")
-			.attr("x", w - 65)
+			.attr("x", w - 20)
 			.attr("y", function(d, i){ return i * 20;})
 			.attr("width", 10)
 			.attr("height", 10)
@@ -90,7 +126,7 @@ function draw_radar_chart( crane_id ){
 			.data(LegendOptions)
 			.enter()
 			.append("text")
-			.attr("x", w - 52)
+			.attr("x", w )
 			.attr("y", function(d, i){ return i * 20 + 9;})
 			.attr("font-size", "11px")
 			.attr("fill", "#737373")
